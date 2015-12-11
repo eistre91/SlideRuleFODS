4dde258ffb9e9a5d44a711b2e8d34397dcd619f5
Erik Istre <e.istre91@gmail.com>
2015-12-11 16:41:29 +1300

    Complete Udacity EDA Problem Set 5 and add work on The Analytics Edge Course Linear Regression Unit

--- Udacity EDA/problemset5.R
+++ Udacity EDA/problemset5.R
@@ -33,3 +33,18 @@ pf <- transform(pf, prop_initiated = ifelse(friend_count > 0, friendships_initia
 head(pf %>% filter(!is.na(prop_initiated)))
 pf %>% filter(!is.na(prop_initiated), prop_initiated > 0, prop_initated < 1)
 
+pf$year_joined <- floor(2014 - pf$tenure/365)
+pf$year_joined.bucket <- cut(pf$year_joined, breaks = c(2004, 2009, 2011, 2012, 2014))
+
+ggplot(aes(x = tenure, y = prop_initiated), data = pf) +
+  geom_line(aes(color=year_joined.bucket))
+
+ggplot(aes(x = 20 * round(tenure / 20), y = prop_initiated), data = filter(pf, prop_initiated > 0, prop_initiated < 1)) +
+  geom_line(aes(color=year_joined.bucket))
+
+filter(pf, prop_initiated > 0, prop_initiated < 1)
+range(pf$prop_initated)
+
+ggplot(aes(x = cut, y = price/carat), data = diamonds) +
+  geom_jitter(aes(color = color)) +
+  facet_wrap( ~ clarity)
